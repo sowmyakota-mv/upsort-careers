1244d79def35c26bd333e6cbae669cbd8d893ce5
sowmyakota-mv <mastersvisa.da31@gmail.com>
2025-07-07 22:38:48 +0530

    Update server.js

--- upsort-backend/server.js
+++ upsort-backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'http://localhost:8080',
+  origin: 'https://upsort-careers-react.onrender.com',
   credentials: true
 }));
 
