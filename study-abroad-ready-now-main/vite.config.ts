3ab62052ecfbd636ff6f4c66aaecc826fd0013ff
sowmyakota-mv <mastersvisa.da31@gmail.com>
2025-07-07 13:42:33 +0530

    Update vite.config.ts

--- study-abroad-ready-now-main/vite.config.ts
+++ study-abroad-ready-now-main/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
   '/api': {
-    target: 'http://localhost:5000',
+    target: 'https://upsort-careers-upsortbackend.onrender.com',
     changeOrigin: true,
     secure: false,
   },
